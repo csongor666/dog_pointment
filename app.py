6a867670063f0f8aa7c27af80018ee9763990a7d
Csongor Báthory <32299927+csongor666@users.noreply.github.com>
2026-09-24 14:36:32 +0200

    Update app.py

--- app.py
+++ app.py
@@ -128,7 +128,7 @@ st.markdown(
             rgba(107,114,128,.48) 78px,
             rgba(107,114,128,.48) 80px
         );
-        background-position:0 0px;
+        background-position:0 73px;
         background-repeat:repeat-y;
     }
     .admin-time-label {
